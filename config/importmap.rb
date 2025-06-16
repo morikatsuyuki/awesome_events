d12a03947fa7406f963efd6e0da58d267beafaf6
ka2yuki-mori <213430954+ka2yuki1987@users.noreply.github.com>
2025-06-16 21:54:57 +0900

    Add Bootstrap using ImportMap

---
+++ config/importmap.rb
@@ -0,0 +1,7 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+pin "jquery",    to: "https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"
+pin "popper",    to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
