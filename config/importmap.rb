90252a9e557d6dc94c85e631a7397870e12dc379
ka2yuki-mori <213430954+ka2yuki1987@users.noreply.github.com>
2025-06-21 12:53:26 +0900

    .

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,6 @@ pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/boots
 pin "jquery",    to: "https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"
 pin "popper",    to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
 
+#　ujsライブラリは importmap と併用する場合：別の方法
 # pin "rails-ujs", to: "https://cdn.jsdelivr.net/npm/rails-ujs@5.2.4/lib/assets/compiled/rails-ujs.js"
 # pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.3/lib/rails-ujs.js"
